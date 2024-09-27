890d3b1e0664af49e987dcc90054456a39113c8e
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-09-27 19:31:29 +0100

    installed tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,21 @@
 
 const Header = () => {
   return (
-    <div>
-      Header page
-    </div>
-  )
-}
+    <>
+      <nav>
+        <div className="container">
+          {/* Logo section */}
+          <div className="text-2xl flex items-center gap-2 font-bold  py-6">
+            <p>NT_Coder</p>
+          </div>
+          {/* Menu section */}
+          {/* Icon section */}
+          {/* Mobile humburger Menu section */}
+        </div>
+      </nav>
+      {/* Mobile sidenar section */}
+    </>
+  );
+};
 
-export default Header
+export default Header;
