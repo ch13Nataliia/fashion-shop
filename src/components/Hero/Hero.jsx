69eec32de2b4223786212c85fc8b2dddd04adad8
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-09-30 12:43:11 +0100

    added motion to hero a collections section on hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,6 @@
 import { FaPlay } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { SlideLeft } from '../../utility/animation';
 // import HeroImg from '../../assets/images/bg-hero-1.jpg'
 const Hero = () => {
   return (
@@ -7,21 +9,41 @@ const Hero = () => {
         {/* Brand info */}
         <div className="flex flex-col justify-center md:py-0 font-playfair">
           <div className="text-center space-y-2">
-            <h1 className="uppercase font-bold text-5xl lg:text-6xl leading-relaxed xl:leading-normal">
+            <motion.h1
+              variants={SlideLeft(0.5)}
+              initial="hidden"
+              animate={'visible'}
+              viewport={{ once: true }}
+              className="uppercase font-bold text-5xl lg:text-6xl leading-relaxed xl:leading-normal"
+            >
               Twinset <span className="text-primary text-sm">milano</span>
-            </h1>
-            <p className="xl:max-w-[500px] text-3xl">Perfect tailoring</p>
-            <p className=" text-2xl"><span className='text-primary'>FW24</span> collection</p>
+            </motion.h1>
+            <motion.p
+              variants={SlideLeft(0.7)}
+              initial="hidden"
+              whileInView={'visible'}
+              viewport={{ once: true }}
+              className="xl:max-w-[500px] text-3xl"
+            >
+              Perfect tailoring
+            </motion.p>
+            <p className=" text-2xl">
+              <span className="text-primary">FW24</span> collection
+            </p>
             {/* button section */}
-            <div className="flex justify-center items-center  gap-8  !mt-4">
+            <motion.div  variants={SlideLeft(0.9)}
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true }}
+            className="flex justify-center gap-6">
               <button className="primary-btn flex items-center gap-2 ">
                 Order NOW
               </button>
-              <button className=" flex gap-2 justify-center items-center">
+              <button className="border-2 border-primary  rounded-lg font-bold  px-5 flex gap-2 justify-center items-center hover:!scale-110 duration-300">
                 <FaPlay />
                 Watch NOW
               </button>
-            </div>
+            </motion.div>
           </div>
         </div>
 
